37340118da68cdf480835f4acaa775e45bafaf2e
sonawanekinjal <kinjalsonawane258@gmail.com>
2023-12-01 18:14:12 +0530

    ygy

--- course/flexpanel.js
+++ course/flexpanel.js
@@ -1,13 +1,15 @@
-const panels = document.querySelectorAll('.panels')
-function panelopen(){
-   console.log("Hello")
-   this.classList.toggle('open')
+const panels = document.querySelectorAll('.panel')
+console.log(panels);
+function panelopen(panel){
+   panel.classList.toggle('open')
 }
-function panelactive(e){
-  console.log(e.propertyName)
+
+function panelactive(e,panel){
+  console.log(e)
   if(e.propertyName.includes('flex')){
-  this.classList.toggle('open-active') 
+    panel.classList.toggle('open-active') 
 }
 } 
-panels.forEach(panel => panel.addEventListener('click',panelopen()))
-panels.forEach(panel => panel.addEventListener('transitioned',panelactive(e)))
+
+panels.forEach(panel => panel.addEventListener('click',()=>panelopen(panel)))
+panels.forEach(panel => panel.addEventListener('transitioned',(e)=>panelactive(e,panel)))
